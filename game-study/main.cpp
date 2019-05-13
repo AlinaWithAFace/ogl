a428fef7fb42d93876b3db8d9dd091a534842fe1
Alina Christenbury <alina.christenbury@gmail.com>
2019-05-13 07:59:31 -0400

    break it down now y'all

--- game-study/main.cpp
+++ game-study/main.cpp
@@ -8,11 +8,13 @@
 
 // Include GLFW
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
+
+GLFWwindow *window;
 
 // Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
 using namespace glm;
 
 #include <common/shader.hpp>
@@ -22,12 +24,59 @@ using namespace glm;
 #include <common/vboindexer.hpp>
 #include <common/text2D.hpp>
 
-int main( void )
-{
+int init();
+
+void update();
+
+void exit();
+
+GLuint VertexArrayID;
+GLuint programID;
+
+GLuint MatrixID;
+GLuint ViewMatrixID;
+GLuint ModelMatrixID;
+
+GLuint Texture;
+GLuint TextureID;
+
+std::vector<glm::vec3> vertices;
+std::vector<glm::vec2> uvs;
+std::vector<glm::vec3> normals;
+
+bool res;
+
+std::vector<unsigned short> indices;
+std::vector<glm::vec3> indexed_vertices;
+std::vector<glm::vec2> indexed_uvs;
+std::vector<glm::vec3> indexed_normals;
+
+GLuint vertexbuffer;
+GLuint uvbuffer;
+GLuint normalbuffer;
+GLuint elementbuffer;
+GLuint LightID;
+
+double lastTime;
+int nbFrames;
+
+
+int main(void) {
+
+	int returnVal = init();
+	if (returnVal != 0) {
+		return returnVal;
+	}
+	update();
+	exit();
+	return 0;
+}
+
+
+int init() {
 	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialize GLFW\n");
 		getchar();
 		return -1;
 	}
@@ -39,9 +88,10 @@ int main( void )
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Tutorial 11 - 2D Fonts", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+	window = glfwCreateWindow(1024, 768, "Tutorial 11 - 2D Fonts", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr,
+				"Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
 		getchar();
 		glfwTerminate();
 		return -1;
@@ -59,12 +109,14 @@ int main( void )
 
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-	// Hide the mouse and enable unlimited mouvement
+	// Hide the mouse and enable unlimited movement
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// Set the mouse at the center of the screen
 	glfwPollEvents();
-	glfwSetCursorPos(window, 1024/2, 768/2);
+	glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+
+
 
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
@@ -77,78 +129,70 @@ int main( void )
 	// Cull triangles which normal is not towards the camera
 	glEnable(GL_CULL_FACE);
 
-	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "StandardShading.vertexshader", "StandardShading.fragmentshader" );
+	programID = LoadShaders("StandardShading.vertexshader", "StandardShading.fragmentshader");
 
 	// Get a handle for our "MVP" uniform
-	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-	GLuint ViewMatrixID = glGetUniformLocation(programID, "V");
-	GLuint ModelMatrixID = glGetUniformLocation(programID, "M");
+	MatrixID = glGetUniformLocation(programID, "MVP");
+	ViewMatrixID = glGetUniformLocation(programID, "V");
+	ModelMatrixID = glGetUniformLocation(programID, "M");
 
 	// Load the texture
-	GLuint Texture = loadDDS("uvmap.DDS");
+	Texture = loadDDS("uvmap.DDS");
 
 	// Get a handle for our "myTextureSampler" uniform
-	GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
+	TextureID = glGetUniformLocation(programID, "myTextureSampler");
 
 	// Read our .obj file
-	std::vector<glm::vec3> vertices;
-	std::vector<glm::vec2> uvs;
-	std::vector<glm::vec3> normals;
-	bool res = loadOBJ("suzanne.obj", vertices, uvs, normals);
+	res = loadOBJ("suzanne.obj", vertices, uvs, normals);
 
-	std::vector<unsigned short> indices;
-	std::vector<glm::vec3> indexed_vertices;
-	std::vector<glm::vec2> indexed_uvs;
-	std::vector<glm::vec3> indexed_normals;
 	indexVBO(vertices, uvs, normals, indices, indexed_vertices, indexed_uvs, indexed_normals);
 
 	// Load it into a VBO
 
-	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, indexed_vertices.size() * sizeof(glm::vec3), &indexed_vertices[0], GL_STATIC_DRAW);
 
-	GLuint uvbuffer;
 	glGenBuffers(1, &uvbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 	glBufferData(GL_ARRAY_BUFFER, indexed_uvs.size() * sizeof(glm::vec2), &indexed_uvs[0], GL_STATIC_DRAW);
 
-	GLuint normalbuffer;
 	glGenBuffers(1, &normalbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
 	glBufferData(GL_ARRAY_BUFFER, indexed_normals.size() * sizeof(glm::vec3), &indexed_normals[0], GL_STATIC_DRAW);
 
 	// Generate a buffer for the indices as well
-	GLuint elementbuffer;
 	glGenBuffers(1, &elementbuffer);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned short), &indices[0], GL_STATIC_DRAW);
 
 	// Get a handle for our "LightPosition" uniform
 	glUseProgram(programID);
-	GLuint LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
+	LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
 
 	// Initialize our little text library with the Holstein font
-	initText2D( "Holstein.DDS" );
+	initText2D("Holstein.DDS");
 
 	// For speed computation
-	double lastTime = glfwGetTime();
-	int nbFrames = 0;
+	lastTime = glfwGetTime();
+	nbFrames = 0;
 
-	do{
+	return 0;
+}
+
+void update() {
+	do {
 
 		// Measure speed
 		double currentTime = glfwGetTime();
 		nbFrames++;
-		if ( currentTime - lastTime >= 1.0 ){ // If last prinf() was more than 1sec ago
+		if (currentTime - lastTime >= 1.0) { // If last prinf() was more than 1sec ago
 			// printf and reset
-			printf("%f ms/frame\n", 1000.0/double(nbFrames));
+			printf("%f ms/frame\n", 1000.0 / double(nbFrames));
 			nbFrames = 0;
 			lastTime += 1.0;
 		}
@@ -172,7 +216,7 @@ int main( void )
 		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
 		glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
 
-		glm::vec3 lightPos = glm::vec3(4,4,4);
+		glm::vec3 lightPos = glm::vec3(4, 4, 4);
 		glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
 
 		// Bind our texture in Texture Unit 0
@@ -190,7 +234,7 @@ int main( void )
 				GL_FLOAT,           // type
 				GL_FALSE,           // normalized?
 				0,                  // stride
-				(void*)0            // array buffer offset
+				(void *) 0            // array buffer offset
 		);
 
 		// 2nd attribute buffer : UVs
@@ -202,7 +246,7 @@ int main( void )
 				GL_FLOAT,                         // type
 				GL_FALSE,                         // normalized?
 				0,                                // stride
-				(void*)0                          // array buffer offset
+				(void *) 0                          // array buffer offset
 		);
 
 		// 3rd attribute buffer : normals
@@ -214,7 +258,7 @@ int main( void )
 				GL_FLOAT,                         // type
 				GL_FALSE,                         // normalized?
 				0,                                // stride
-				(void*)0                          // array buffer offset
+				(void *) 0                          // array buffer offset
 		);
 
 		// Index buffer
@@ -225,7 +269,7 @@ int main( void )
 				GL_TRIANGLES,      // mode
 				indices.size(),    // count
 				GL_UNSIGNED_SHORT, // type
-				(void*)0           // element array buffer offset
+				(void *) 0           // element array buffer offset
 		);
 
 		glDisableVertexAttribArray(0);
@@ -233,7 +277,7 @@ int main( void )
 		glDisableVertexAttribArray(2);
 
 		char text[256];
-		sprintf(text,"%.2f sec", glfwGetTime() );
+		sprintf(text, "%.2f sec", glfwGetTime());
 		printText2D(text, 10, 500, 60);
 
 		// Swap buffers
@@ -241,9 +285,11 @@ int main( void )
 		glfwPollEvents();
 
 	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0);
+}
 
+void exit() {
 	// Cleanup VBO, shader and texture
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteBuffers(1, &uvbuffer);
@@ -258,7 +304,4 @@ int main( void )
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
-
-	return 0;
 }
-
